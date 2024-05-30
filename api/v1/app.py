56707795563f99e9359157e2d363f26dddcdf8ae
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-30 13:37:03 +0200

    API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     if not host:
         host = '0.0.0.0'
     if not port:
-        port = '5000'
+        port = '5001'
     app.run(host=host, port=port, threaded=True)
